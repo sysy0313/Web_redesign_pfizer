d700faab9cb998e43d6d792a1b9cd20f9460c3f5
soyeon <sysy0313@gmail.com>
2023-08-30 00:28:33 +0900

    0829 home save

--- script/common.js
+++ script/common.js
@@ -1,6 +1,22 @@
+// header
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset == 0){
+        header.style.background = 'rgba(255,255,255,0.7)'
+        sub_bg.style.background = 'rgba(255,255,255,0.7)'
+        search_open.style.background = 'rgba(255,255,255,0.7)'
+    }else{
+        header.style.background = '#fff'
+        sub_bg.style.background = "#fff"
+        search_open.style.background = '#fff'
+    }
+})
+
+
+const header = document.querySelector('header')
 const sub_bg = document.querySelector('.sub_bg')
 const gnb_sub = document.querySelectorAll('#gnb .sub')
-const gnb_li = document.querySelectorAll('#gnb > li')
+const gnb_li = document.querySelectorAll('#gnb > li > a')
+const nav = document.querySelector('nav')
 // 모바일메뉴
 const m_nav_open = document.querySelector('.m_nav_open')
 const m_nav_btn = document.querySelector('.m_nav_btn')
@@ -9,19 +25,16 @@ const m_nav_sub = document.querySelectorAll('.m_nav_open .sub')
 //검색
 const header_search = document.querySelector('.header_search')
 const search_open = document.querySelector('.search_open')
-// search_open.style.display = 'none'
 
-console.log(gnb_li)
-
-// sub
-$('#gnb .sub').hide();
-$('.sub_bg').hide();
-$('nav').mouseover(function(){
-    $('#gnb .sub').stop().slideDown(500)
-    $('.sub_bg').stop().slideDown(500)
-}).mouseout(function(){
-    $('#gnb .sub').stop().slideUp(500)
-    $('.sub_bg').stop().slideUp(500)
+for(let i of gnb_li){i.addEventListener('mouseover', function(){
+    sub_bg.classList.add('open')
+    for(let i of gnb_sub){i.classList.add('open')}
+    header.classList.add('sub')
+})}
+sub_bg.addEventListener('mouseout', function(){
+    sub_bg.classList.remove('open')
+    for(let i of gnb_sub){i.classList.remove('open')}
+    header.classList.remove('sub')
 })
 
 // m_nav open
@@ -29,6 +42,9 @@ m_nav_btn.addEventListener('click',function(e){
     e.preventDefault();
     search_open.classList.remove('open')
     m_nav_open.classList.toggle('open')
+    header.classList.toggle('open')
+    sub_bg.classList.toggle('hide')
+    for(let i of gnb_sub){i.classList.toggle('hide')}
 })
 // m_nav sub
 m_nav_li.forEach(function(target, index){
@@ -43,4 +59,7 @@ header_search.addEventListener('click',function(e){
     e.preventDefault();
     m_nav_open.classList.remove('open')
     search_open.classList.toggle('open')
+    sub_bg.classList.toggle('hide')
+    for(let i of gnb_sub){i.classList.toggle('hide')}
+    header.classList.toggle('sub')
 })
--- script/index.js
+++ script/index.js
@@ -5,6 +5,7 @@ $('#wrap').fullpage({
     navigation:true,
     navigationTooltips:['main','연구제품','제약제품','최신소식','사회공헌']
 })
+// visual_slide
 const visual_slide = new Swiper('#visual_slide',{
     autoplay:{
         delay:8000,
@@ -13,4 +14,46 @@ const visual_slide = new Swiper('#visual_slide',{
     loop:true, //마지막슬라이드->첫슬라이드 자연스럽게
     effect:'fade',// effect:'fade', //제자리 자연스러운 변경 슬라이드 효과
     
+});
+// drug_hover
+const drug_box = document.querySelectorAll('.drug .contents a')
+const hover_p = document.querySelectorAll('.drug .contents a .hover')
+const icon_show = document.querySelectorAll('.drug .contents a .icon_show')
+const icon_hover = document.querySelectorAll('.drug .contents a .icon_hover')
+for(let i of hover_p){i.style.display = 'none'}
+for(let i of icon_hover){i.style.display = 'none'}
+drug_box.forEach(function(target, index){
+    target.addEventListener('mouseover', function(){
+        hover_p[index].style.display ='block'
+        icon_show[index].style.display = 'none'
+        icon_hover[index].style.display = 'block'
+    });
+    target.addEventListener('mouseout', function(){
+        hover_p[index].style.display = 'none'
+        icon_show[index].style.display = 'block'
+        icon_hover[index].style.display = 'none'
+    })
+})
+//news_slide
+const news_slide = new Swiper('#news_slide',{
+    autoplay:{
+        delay:2000,
+        disableOnInteraction:false,//버튼 클릭 후 자동재생유지
+    }, //자동재생
+    loop:true, //마지막슬라이드->첫슬라이드 자연스럽게
+    effect:'horizontal',// 
+    slidesPerView:3,
+    spaceBetween:30,
+    
+});
+//contribution_slide
+const contribution_slide = new Swiper('#contribution_slide',{
+    autoplay:{
+        delay:8000,
+        disableOnInteraction:false,//버튼 클릭 후 자동재생유지
+    }, //자동재생
+    loop:true, //마지막슬라이드->첫슬라이드 자연스럽게
+    effect:'horizontal',
+    slidesPerView:3,
+    spaceBetween:30,
 });
